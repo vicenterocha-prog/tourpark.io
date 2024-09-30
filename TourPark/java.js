4bef564d17bda00593c133158ece809b267b8331
Vicente Rocha <rocha66@gmail.com>
2024-09-29 22:15:19 -0300

    Upload de tourpark

---
+++ TourPark/java.js
@@ -0,0 +1,14 @@
+
+
+
+document.addEventListener('DOMContentLoaded', () => {
+    console.log ('Script carregado');
+});
+
+
+        
+    
+
+    
+
+
---
+++ TourPark/java1.js
@@ -0,0 +1,4 @@
+document.addEventListener('DOMContentLoaded', () => {
+    console.log('Página carregada e pronta!');
+});
+
